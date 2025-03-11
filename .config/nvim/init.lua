ddd8b13cca2416117e6fa219d72d44fc94c16d46
lucastcottle <lucas.t.cottle@gmai.com>
2025-03-11 21:56:59 +1300

    we gruvbox now

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -860,17 +860,15 @@ require('lazy').setup({
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'neanias/everforest-nvim',
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme.
+    'sainnhe/gruvbox-material',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      require('everforest').setup {
-        background = 'hard',
-      }
       --r)oad the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'everforest'
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme 'gruvbox-material'
       vim.opt.termguicolors = true
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
