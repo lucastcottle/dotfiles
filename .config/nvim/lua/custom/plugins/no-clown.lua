0354d82aeb23032b4ad9387219952b0d8ec74eee
lucastcottle <lucas.cottle@datazoo.com>
2025-10-23 16:20:31 +1300

    no clown time

---
+++ .config/nvim/lua/custom/plugins/no-clown.lua
@@ -0,0 +1,20 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'aktersnurra/no-clown-fiesta.nvim',
+  priority = 1000, -- Load colorscheme early
+  config = function()
+    require('no-clown-fiesta').setup {
+      -- your options here, e.g.:
+      theme = 'dark',
+      styles = {
+        type = { bold = true },
+        lsp = { underline = false },
+        match_paren = { underline = true },
+      },
+    }
+    vim.cmd.colorscheme 'no-clown-fiesta'
+  end,
+}
