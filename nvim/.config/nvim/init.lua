8899a35a5ea48b802886c94188a0199da9a20824
lucastcottle <lucas.t.cottle@gmail.com>
2026-08-24 22:19:45 +1200

    nvim updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -805,12 +805,12 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
+  --[[ { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    --'folke/tokyonight.nvim'],
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -825,7 +825,7 @@ require('lazy').setup({
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
     end,
-  },
+  }, ]]
 
   -- Highlight todo, notes, etc in comments
   {
